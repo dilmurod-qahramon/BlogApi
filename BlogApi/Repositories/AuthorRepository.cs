fe8a564519e3f5485c4dcc8b5ab8723911e85530
Dilmurod Rozikov <drozikov@amcbridge.com>
2025-03-18 11:04:41 +0500

    001: set up generic repository

---
+++ BlogApi/Repositories/AuthorRepository.cs
@@ -0,0 +1,15 @@
+﻿using BlogApi.DataAccess;
+using BlogApi.Models;
+using BlogApi.Repositories.interfaces;
+
+namespace BlogApi.Repositories
+{
+    public class AuthorRepository : Repository<Author>, IAuthorRepository
+    {
+        private readonly BlogDbContext _blogDbContext;
+        public AuthorRepository(BlogDbContext blogDbContext) : base(blogDbContext)
+        {
+            _blogDbContext = blogDbContext;
+        }
+    }
+}
---
+++ BlogApi/Repositories/interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogApi.Repositories.interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
